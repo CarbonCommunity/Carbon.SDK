04a18dfe4c36b55355843323e122f06dd7af8161
raul <raulssorban@gmail.com>
2023-09-22 01:38:04 +0200

    Added `ModuleEventArgs`

---
+++ src/Events/Classes/ModuleEventArgs.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using API.Assembly;
+
+/*
+ *
+ * Copyright (c) 2022-2023 Carbon Community 
+ * All rights reserved.
+ *
+ */
+
+namespace API.Events;
+
+public class ModuleEventArgs : CarbonEventArgs
+{
+	public ICarbonModule Module;
+	public IEnumerable<object> Data;
+
+	public ModuleEventArgs(object payload, ICarbonModule module, IEnumerable<object> data) : base(payload)
+	{
+		Module = module;
+		Data = data;
+	}
+}
